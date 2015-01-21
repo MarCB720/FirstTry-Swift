38501fb60d9fd9801c26496943c657aef573e1a5
marcB_III <marcb.mac17@gmail.com>
2015-01-21 18:08:33 -0500

    Initial Commit

---
+++ GitHubFirstTry1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitHubFirstTry1
+//
+//  Created by marcB_III on 1/21/15.
+//  Copyright (c) 2015 marcB_III. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do any additional setup after loading the view, typically from a nib.
+   }
+
+   override func didReceiveMemoryWarning() {
+      super.didReceiveMemoryWarning()
+      // Dispose of any resources that can be recreated.
+   }
+
+
+}
+
